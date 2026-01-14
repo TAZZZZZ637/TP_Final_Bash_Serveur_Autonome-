6ffbb7d4243e3c84d2ffd14c05b4e0b23d387f58
yanis77 <perotyanis@gmail.com>
2026-01-14 15:44:48 +0100

    Update serveur_autonome.sh

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -6,7 +6,8 @@ SWAP_THRESHOLD=90
 
 echo "WARNING : utilisation disque critique (${DISK_USAGE}%)"
 echo "WARNING : utilisation SWAP critique (${SWAP_USAGE}%)"
-fi
 
 ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4
 ps -eo pid,comm,%mem --sort=-%mem | head -n 4
+
+fi

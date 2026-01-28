d4ecd78494f6f4e3af4261766dfb0053b2ef9f1d
yanis77 <perotyanis@gmail.com>
2026-01-28 09:36:23 +0100

    Update serveur_autonome.sh

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -10,4 +10,22 @@ echo "WARNING : utilisation SWAP critique (${SWAP_USAGE}%)"
 ps -eo pid,comm,%cpu --sort=-%cpu | head -n 4
 ps -eo pid,comm,%mem --sort=-%mem | head -n 4
 
+services=("sshd" "postgres" "apache2")
+
+echo "--- Début de la vérification des services : $(date) ---"
+for service in "${services[@]}"; do
+    if systemctl is-active --quiet "$service"; then
+        echo "OK : Le service [$service] fonctionne correctement."
+    else
+        echo "WARN : [$service] est inactif. Tentative de redémarrage..."
+        systemctl start "$service"
+        sleep 1
+        if systemctl is-active --quiet "$service"; then
+            echo "RECOVERY : [$service] a été redémarré avec succès."
+        else
+            echo "ERROR : Échec du redémarrage pour [$service]."
+        fi
+        fi
+done
+echo "--- Fin du rapport ---"
 fi
